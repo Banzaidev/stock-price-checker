bc1a08bdfb0cfe94376860a40bfa919bbdf5b650
Banzaidev <66788936+Banzaidev@users.noreply.github.com>
2025-06-07 18:20:12 +0200

    feat: add GET /api/stock-prices route to fetch stock data and handle likes

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,21 @@
 'use strict';
-
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
-    .get(function (req, res){
-      
+    .get(async (req, res) => {
+      let symbol = req.query.stock
+      let isLike = req.query.like
+      if((isLike != 'true' && isLike != 'false') && isLike != undefined){
+        return res.status(400).send('should be true or false')
+      }
+      const apiData = await fetch(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${symbol}/quote`).then(data => data.json())
+      let data = {
+        "stockData": {"stock":apiData.symbol,"price":apiData.latestPrice,"likes":0}
+      }
+      if (isLike == 'true'){
+        data.stockData.likes =  data.stockData.likes + 1
+      }
+      res.json(data)
     });
     
 };
